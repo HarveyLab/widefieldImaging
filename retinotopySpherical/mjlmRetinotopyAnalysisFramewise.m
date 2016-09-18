80b050635190a29e779218a889fc8f88c50c6a08
mjlm-intrinsicScope <m.minderer@gmail.com>
2016-09-18 13:55:56 -0400

    Change to save directly to server

--- retinotopySpherical/mjlmRetinotopyAnalysisFramewise.m
+++ retinotopySpherical/mjlmRetinotopyAnalysisFramewise.m
@@ -3,8 +3,8 @@ function results = mjlmRetinotopyAnalysisFramewise
 %% Settings:
 % base = '\\research.files.med.harvard.edu\Neurobio\HarveyLab\Matthias\data\imaging\widefield';
 base = 'D:\Data\Matthias';
-movFolder = fullfile(base, 'MM104_160729_retino');
-datFile = fullfile(base, '20160729_190439_retinotopy_MM104.mat');
+movFolder = fullfile(base, 'MM102_160907_retino');
+datFile = fullfile(base, '20160907_203252_retinotopy_MM102.mat');
 
 %% Get movie metadata:
 dat = load(datFile);
@@ -24,17 +24,19 @@ for iFile = 1:nFiles
     str = regexp(lst(iFile).name, '(?<=_)()\d{4,5}(?=\.)', 'match', 'once');
     fileNameNumber(iFile) = str2double(str);
 end
-nFramesInSyncStruct = numel(dat.frame.past.frameId);
 [~, order] = sort(fileNameNumber);
 lst = lst(order);
-lst = lst(1:nFramesInSyncStruct);
 
 img = imread(fullfile(movFolder, lst(1).name));
 dat.mov.height = size(img, 1);
 dat.mov.width = size(img, 2);
-dat.mov.nFrames = nFramesInSyncStruct;
+dat.mov.nFramesStim = numel(dat.frame.past.frameId);
+dat.mov.nFramesCam = numel(lst);
 dat.mov.nPixPerFrame = dat.mov.height * dat.mov.width;
 temporalDownSampling = 1;
+
+assert(abs(sum(dat.frame.past.isCamTriggerFrame)-dat.mov.nFramesCam)<=1, ...
+    'Recorded number of images does not match sync data!')
 
 %% Get condition data:
 uniqueDir = unique(dat.frame.past.barDirection_deg);
@@ -58,8 +60,9 @@ for iCond = 1:nCond
     results(iCond).period = results(iCond).nFrames / results(iCond).nReps;
     
     barPosBinnedHere = barPosBinned(dat.frame.past.barDirection_deg==results(iCond).dir);
+    camTriggersHere = dat.frame.past.isCamTriggerFrame(dat.frame.past.barDirection_deg==results(iCond).dir);
     results(iCond).uniqueBins = unique(barPosBinnedHere);
-    results(iCond).nInBin = accumarray(barPosBinnedHere', ones(size(barPosBinnedHere')));
+    results(iCond).nInBin = accumarray(barPosBinnedHere', camTriggersHere');
     results(iCond).tuning = zeros(dat.mov.height, dat.mov.width, nBins);
 end
 
@@ -70,7 +73,7 @@ end
 
 % For motion correction, use center frame as reference:
 % (SNR is good enough to just use one frame.)
-ref = double(imread(fullfile(movFolder, lst(round(nFramesInSyncStruct/2)).name)));
+ref = double(imread(fullfile(movFolder, lst(round(dat.mov.nFramesCam/2)).name)));
 ref(ref<1000) = ref(ref<1000) + 2^16; % Fix overflow
 
 % Pre-calculate reference FFT:
@@ -80,15 +83,26 @@ ref_fft(:,:,2) = fftshift(ref_fft(:,:,1));
 ref_fft(:,:,3) = conj(ref_fft(:,:,1));
 
 ticProc = tic;
+nFilesProcessed = 0;
 for iCond = 1:nCond
+    
+    % Get index of all STIMULUS frames for this condition:
     thisCondFrameInd = find(dat.frame.past.barDirection_deg==results(iCond).dir);
+    
     for iFrame = thisCondFrameInd
+        % Skip the stimulus frames on which no image was triggered:
+        if ~dat.frame.past.isCamTriggerFrame(iFrame)
+            continue
+        else
+            iFile = sum(dat.frame.past.isCamTriggerFrame(1:iFrame));
+        end
+        
         % Load frame:
-        imgHere = double(imread(fullfile(movFolder, lst(iFrame).name)));
+        imgHere = double(imread(fullfile(movFolder, lst(iFile).name)));
         imgHere(imgHere<1000) = imgHere(imgHere<1000) + 2^16; % Fix occasional overflow
         
         % Correct motion:
-        [imgHere, dat.mov.shifts.x(iFrame), dat.mov.shifts.y(iFrame)] = ...
+        [imgHere, dat.mov.shifts.x(iFile), dat.mov.shifts.y(iFile)] = ...
             correct_translation_singleframe(imgHere, ref_fft, 1);
         
         % Add current frame to the appropriate bin:
@@ -96,8 +110,10 @@ for iCond = 1:nCond
         results(iCond).tuning(:,:,iBin) = results(iCond).tuning(:,:,iBin) ...
             + imgHere ./ results(iCond).nInBin(iBin);
         
-        fprintf('Processing frame % 6.0f/% 1.0f (%1.1f minutes left)\n', ...
-            iFrame, nFramesInSyncStruct, (toc(ticProc)/(60*iFrame)) * (nFramesInSyncStruct-iFrame));
+        nFilesProcessed = nFilesProcessed+1;
+        fprintf('Processing file % 6.0f/% 1.0f (%1.1f minutes left)\n', ...
+            nFilesProcessed, dat.mov.nFramesCam, (toc(ticProc)/(60*nFilesProcessed)) * ...
+            (dat.mov.nFramesCam-nFilesProcessed));
     end
 end
 
--- retinotopySpherical/retinotopySpherical.m
+++ retinotopySpherical/retinotopySpherical.m
@@ -7,18 +7,30 @@
 %% User settings:
 isDebug = false;
 settings = struct;
-settings.saveDir = 'D:\Data\Matthias';
-settings.expName = char(inputdlg('Experiment Name? '));
+settings.mouseName = char(inputdlg('Enter mouse name:'));
+
+% Tested saving video directly to server. It works.
+% settings.saveDir = 'D:\Data\Matthias';
+folderName = [settings.mouseName, '_', ...
+    datestr(now, 'yymmdd'), ...
+    '_retino'];
+settings.saveDir = fullfile('\\research.files.med.harvard.edu\neurobio\HarveyLab\Matthias\data\imaging\widefield', ...
+    settings.mouseName, folderName);
+if ~exist(settings.saveDir, 'dir')
+    mkdir(settings.saveDir)
+end
+
 % 10 reps was not enough with transgenic. Try 20.
 settings.nRepeats = 50; % How often each direction is repeated, i.e. there will be 4 times as many sweeps. Garrett uses 6-10 times 10, so up to 100 sweeps!
-settings.nRepeatsPerBlock = 36;
+settings.nRepeatsPerBlock = 6;
 settings.barWidth_deg = 10; % Marshel uses 20
 settings.barSpeed_dps = 10; % Marshel uses 8.5-9.5 dps
 settings.checkerBlink_hz = 6; % Marshel uses 6 Hz
 settings.minDistEyeToScreen_mm = 130;
-settings.screenOri_xyPix = [-104, 35];
+settings.screenOri_xyPix = [-96, 47];
 settings.pixelReductionFactor = 5; % How much the texture is downsampled...affects frame rate.
-settings.fps = 60; % Target display/acquisition rate. Max is 120 Hz (monitor refresh)
+settings.fpsStim = 60; % Target display/acquisition rate. Max is 120 Hz (monitor refresh)
+settings.camFrameStride = 2; % The camera takes one picture every this many frames.
 
 % Empirical duration:
 screenWidthDegTheoretical = 132.7;
@@ -26,7 +38,7 @@ screenHeightDegTheoretical = 104.5;
 expDur = settings.nRepeats * ...
     4 * ... % Number of conditions/bar directions
     (screenWidthDegTheoretical+screenHeightDegTheoretical)*0.5/settings.barSpeed_dps;
-button = questdlg(sprintf('Experiment will take about %1.1f minutes. Click YES to start.', ...
+button = questdlg(sprintf('Experiment will take about %1.1f minutes. Start video recording, then click YES to start.', ...
     expDur/60));
 if ~strcmp(button, 'Yes')
     return
@@ -78,6 +90,7 @@ screen.isAntiAliasing = 0;
 frameStruct = struct;
 frameStruct.frameId = 0;
 frameStruct.flipTime_s = 0;
+frameStruct.isCamTriggerFrame = 0;
 frameStruct.barDirection_deg = 0;
 frameStruct.barPosition_deg = 0;
 frame = eventSeries(frameStruct);
@@ -100,7 +113,7 @@ screen.fullRect = Screen('Rect', screen.win);
 % Set alpha blending settings (for fading to black at the end of a trial).
 % GL_colormask = [0 1 1 0]; % RGBA mask to exclude red and green light.
 % Screen('BlendFunction', screen.win, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_colormask);
-Screen('FillRect', screen.win, [127 127 127])
+Screen('FillRect', screen.win, [0 0 127])
 Screen('Flip', screen.win);
 outputSingleScan(camControl,[1 1 0]) %LEDs on
 pause(5); % For luminance adaptation.
@@ -110,8 +123,8 @@ pause(5); % For luminance adaptation.
 %Draw first streen to ensure functions are all loaded:
 tex = prepareSphericalBarTex(screen, settings, frame.next.barDirection_deg);
 texMat = makeSphericalBar(tex, settings, 0, frame.next.barDirection_deg);
-% texMat = repmat(texMat, 1, 1, 3);
-% texMat(:,:,1:2) = 0;
+texMat = repmat(texMat, 1, 1, 3);
+texMat(:,:,1:2) = 0;
 texPtr = Screen('MakeTexture', screen.win, texMat);
 Screen('DrawTexture', screen.win, texPtr, [], screen.fullRect);
 Screen('Flip', screen.win);
@@ -153,22 +166,26 @@ while (nRepeatsDone < settings.nRepeats) && ~isExit
             % Draw frame:
             [texMat, frame.next.barPosition_deg] = ...
                 makeSphericalBar(tex, settings, timeInCondition_s, frame.next.barDirection_deg);
-            %         texMat = repmat(texMat, 1, 1, 3);
-            %         texMat(:,:,1:2) = 0;
+            texMat = repmat(texMat, 1, 1, 3);
+            texMat(:,:,1:2) = 0;
             texPtr = Screen('MakeTexture', screen.win, texMat);
             Screen('DrawTexture', screen.win, texPtr, [], screen.fullRect);
             
             % Advance frame:
             frame.next.frameId = frame.current.frameId + 1;
             frame.next.flipTime_s = Screen('Flip', screen.win, ...
-                frame.current.flipTime_s+(0.75/settings.fps));
+                frame.current.flipTime_s+(0.75/settings.fpsStim));
             timeInCondition_s = frame.next.flipTime_s - t0;
             advance(frame);
             
             % Trigger camera acquisition:
-            if ~isDebug
+            if ~isDebug && ...
+                    mod(frame.current.frameId-1, settings.camFrameStride)==0
                 outputSingleScan(camControl,[1 1 1]) % Trigger
                 outputSingleScan(camControl,[1 1 0]) % Reset
+                frame.current.isCamTriggerFrame = 1;
+            else
+                frame.current.isCamTriggerFrame = 0;
             end
             
             % Clean up:
@@ -192,7 +209,7 @@ if ~exist(settings.saveDir, 'dir')
     mkdir(settings.saveDir);
 end
 saveFileName = fullfile(settings.saveDir, ...
-    [datestr(now, 'yyyymmdd_HHMMSS'), '_retinotopy_', settings.expName]);
+    [datestr(now, 'yyyymmdd_HHMMSS'), '_retinotopy_', settings.mouseName]);
 
 mfile = fileread(which(mfilename));
 save(saveFileName, 'settings', 'screen', 'frame', 'mfile')
